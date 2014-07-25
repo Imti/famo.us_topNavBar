b74c2048af6d57edb29b2d54be6f063e69d5492e
Imti <imtiazmaj@gmail.com>
2014-07-25 01:15:49 -0700

    Set up basic Famo.us file structure

---
+++ src/main.js
@@ -0,0 +1,10 @@
+define(function(require, exports, module) {
+  // Requires
+  var Engine = require('famous/core/Engine');
+  var Surface = require('famous/core/Surface');
+  var Modifier = require('famous/core/Modifier');
+  var Transform = require('famous/core/Transform');
+
+  var mainContext = Engine.createContext();
+
+});
